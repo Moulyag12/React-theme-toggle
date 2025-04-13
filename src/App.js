45d41be24619166ac79bfd10f3d021794631331a
Moulya <moulyagreddy@gmail.com>
2025-04-13 16:17:18 +0530

    theme toggle initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React,{useEffect, useState} from "react";
+import './App.css';
 function App() {
+  const[theme,setTheme]=useState(()=>{
+    return localStorage.getItem('theme') || 'light';
+    })
+
+    const toggleTheme=()=>{
+      setTheme(prev=>(prev==="light"?"dark":"light"));
+    };
+    useEffect(() => {
+      if (typeof window !== 'undefined') {
+        document.body.classList.remove('light', 'dark');
+        document.body.classList.add(theme);
+        localStorage.setItem('theme', theme);
+      }
+    }, [theme]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{theme === 'light' ? '🌞 Light Mode' : '🌙 Dark Mode'}</h1>
+      <button onClick={()=>toggleTheme()}>
+        Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
+      </button>
     </div>
   );
 }
